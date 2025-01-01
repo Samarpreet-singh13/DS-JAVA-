1ea3a103f8e6da22d42e416abe3766d28102aa57
Samarpreet Singh <psamar142@gmail.com>
2025-01-01 23:45:29 +0530

    new commit

--- Graphs/Bipartite.java
+++ Graphs/Bipartite.java
@@ -3,22 +3,23 @@ package Graphs;
 import java.util.*;
 
 public class Bipartite {
-    static class Edge{
+    static class Edge {
         int src;
         int dest;
 
-        public Edge(int s,int d){
-            this.src=s;
-            this.dest=d;
+        public Edge(int s, int d) {
+            this.src = s;
+            this.dest = d;
         }
     }
-     public static void createGraph(ArrayList<Edge>[]graph){
+
+    public static void createGraph(ArrayList<Edge>[] graph) {
         for (int i = 0; i < graph.length; i++) {
-            graph[i]=new ArrayList<>();
+            graph[i] = new ArrayList<>();
         }
         graph[0].add(new Edge(0, 1));
         graph[0].add(new Edge(0, 2));
-        
+
         graph[1].add(new Edge(1, 0));
         graph[1].add(new Edge(1, 3));
 
@@ -31,29 +32,30 @@ public class Bipartite {
         graph[4].add(new Edge(4, 2));
         graph[4].add(new Edge(4, 3));
     }
-    public static boolean isBipartite(ArrayList<Edge>[]graph){
-        int col[]=new int[graph.length];
+
+    public static boolean isBipartite(ArrayList<Edge>[] graph) {
+        int col[] = new int[graph.length];
         for (int i = 0; i < col.length; i++) {
-            col[i]=-1;
+            col[i] = -1;
         }
-        Queue<Integer>q=new LinkedList<>();
+        Queue<Integer> q = new LinkedList<>();
         for (int i = 0; i < graph.length; i++) {
             // marking as uncoloured
-            if (col[i]==-1) {
+            if (col[i] == -1) {
                 q.add(i);
                 // marking a color to push in q and col array
-                col[i]=0;
+                col[i] = 0;
                 while (!q.isEmpty()) {
-                    int curr=q.remove();
+                    int curr = q.remove();
                     for (int j = 0; j < graph[curr].size(); j++) {
-                        Edge e=graph[curr].get(j);
+                        Edge e = graph[curr].get(j);
                         // chehcking if the nbr of the curr node is colord or not
-                        if (col[e.dest]==-1) {//if not marked then the nbr is marked according the curr
-                            int nexcol=col[curr]==0?1:0;
-                            col[e.dest]=nexcol;
+                        if (col[e.dest] == -1) {// if not marked then the nbr is marked according the curr
+                            int nexcol = col[curr] == 0 ? 1 : 0;
+                            col[e.dest] = nexcol;
                             q.add(e.dest);
                             // if the col of curr is same as the col of nbr the false is returned
-                        }else if (col[e.dest]==col[curr]) {
+                        } else if (col[e.dest] == col[curr]) {
                             return false;
                         }
                     }
@@ -62,9 +64,10 @@ public class Bipartite {
         }
         return true;
     }
+
     public static void main(String[] args) {
-        int v=7;
-        ArrayList<Edge>[]graph=new ArrayList[v];
+        int v = 7;
+        ArrayList<Edge>[] graph = new ArrayList[v];
         createGraph(graph);
         System.out.println(isBipartite(graph));
     }
