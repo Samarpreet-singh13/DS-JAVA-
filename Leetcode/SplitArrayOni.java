83f60cfca615918e45ffd8729a5bdedc6d2652d4
Samarpreet Singh <psamar142@gmail.com>
2025-01-04 00:48:05 +0530

    leetocde 2270

---
+++ Leetcode/SplitArrayOni.java
@@ -0,0 +1,21 @@
+package Leetcode;
+
+public class SplitArrayOni {
+    // public int waysToSplitArray(int[] nums) {
+    //     long sum=0;
+    //     long left=0;
+    //     for(int i=0;i<nums.length;i++){
+    //         sum=sum+nums[i];
+    //     }
+    //     int count=0;
+    //     long right=sum;
+    //     for(int i =0;i<nums.length-1;i++){
+    //         left=left+nums[i];
+    //         right=right-nums[i];
+    //         if(left>=right){
+    //             count++;
+    //         }
+    //     }
+    //     return count;
+    // }
+}
